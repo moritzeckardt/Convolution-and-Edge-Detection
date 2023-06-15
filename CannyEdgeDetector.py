9c6c43e609b4ab56c579289b6a8af477c080ae80
Jonas Busch <92538801+buscjona@users.noreply.github.com>
2023-06-15 22:24:20 +0200

    Add files via upload

--- CannyEdgeDetector.py
+++ CannyEdgeDetector.py
@@ -16,9 +16,18 @@ def gaussFilter(img_in, ksize, sigma):
     :param sigma: sigma (float)
     :return: (kernel, filtered) kernel and gaussian filtered image (both np.ndarray)
     """
-    # TODO
+    # Create a 1D Gaussian kernel
+    kernel_1d = np.linspace(-(ksize // 2), ksize // 2, ksize)
+    kernel_1d = np.exp(-0.5 * (kernel_1d / sigma) ** 2)
+    kernel_1d /= np.sum(kernel_1d)
 
-    pass
+    # Create a 2D Gaussian kernel by multiplying the 1D kernel with its transpose
+    kernel = np.outer(kernel_1d, kernel_1d)
+
+    # Perform convolution using scipy.ndimage.convolve
+    filtered = convolve(img_in, kernel)
+
+    return kernel, filtered.astype(int)
 
 
 def sobel(img_in):
@@ -29,8 +38,13 @@ def sobel(img_in):
     :param img_in: input image (np.ndarray)
     :return: gx, gy - sobel filtered images in x- and y-direction (np.ndarray, np.ndarray)
     """
-    # TODO
-    pass
+    kernel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+    kernel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+    gx = convolve(img_in, kernel_x)
+    gy = convolve(img_in, kernel_y)
+
+    return gx.astype(int), gy.astype(int)
 
 
 def gradientAndDirection(gx, gy):
@@ -40,8 +54,10 @@ def gradientAndDirection(gx, gy):
     :param gy: sobel filtered image in x direction (np.ndarray)
     :return: g, theta (np.ndarray, np.ndarray)
     """
-    # TODO
-    pass
+    gradient = np.sqrt(gx ** 2 + gy ** 2)
+    theta = np.arctan2(gy, gx)
+
+    return gradient.astype(int), theta
 
 
 def convertAngle(angle):
@@ -50,8 +66,15 @@ def convertAngle(angle):
     :param angle: in radians
     :return: nearest match of {0, 45, 90, 135}
     """
-    # TODO
-    pass
+    angle = np.rad2deg(angle) % 180
+    if angle < 22.5 or angle >= 157.5:
+        return 0
+    elif angle < 67.5:
+        return 45
+    elif angle < 112.5:
+        return 90
+    else:
+        return 135
 
 
 def maxSuppress(g, theta):
@@ -61,8 +84,27 @@ def maxSuppress(g, theta):
     :param theta: 2d image (np.ndarray)
     :return: max_sup (np.ndarray)
     """
-    # TODO Hint: For 2.3.1 and 2 use the helper method above
-    pass
+    rows, cols = g.shape
+    suppressed = np.zeros_like(g)
+
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            angle = convertAngle(theta[i, j])
+
+            if angle == 0:
+                if g[i, j] >= g[i, j - 1] and g[i, j] >= g[i, j + 1]:
+                    suppressed[i, j] = g[i, j]
+            elif angle == 45:
+                if g[i, j] >= g[i - 1, j + 1] and g[i, j] >= g[i + 1, j - 1]:
+                    suppressed[i, j] = g[i, j]
+            elif angle == 90:
+                if g[i, j] >= g[i - 1, j] and g[i, j] >= g[i + 1, j]:
+                    suppressed[i, j] = g[i, j]
+            else:
+                if g[i, j] >= g[i - 1, j - 1] and g[i, j] >= g[i + 1, j + 1]:
+                    suppressed[i, j] = g[i, j]
+
+    return suppressed
 
 
 def hysteris(max_sup, t_low, t_high):
@@ -76,8 +118,32 @@ def hysteris(max_sup, t_low, t_high):
     :param t_high: (int)
     :return: hysteris thresholded image (np.ndarray)
     """
-    # TODO
-    pass
+    rows, cols = max_sup.shape
+    result = np.zeros_like(max_sup)
+    strong = max_sup >= t_high
+    weak = (max_sup >= t_low) & (max_sup < t_high)
+
+    result[strong] = 255
+    visited = set()
+
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            if (i, j) in visited:
+                continue
+
+            if weak[i, j]:
+                visited.add((i, j))
+                neighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1),
+                             (i - 1, j - 1), (i - 1, j + 1), (i + 1, j - 1), (i + 1, j + 1)]
+
+                for neighbor in neighbors:
+                    ni, nj = neighbor
+                    if (ni, nj) not in visited and strong[ni, nj]:
+                        result[i, j] = 255
+                        visited.add((i, j))
+                        break
+
+    return result
 
 
 def canny(img):
