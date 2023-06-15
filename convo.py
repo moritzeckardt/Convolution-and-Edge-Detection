afdbcf71ca4441bb7ffbb7acaf5070a17dfad1f6
Moritz Eckardt <moritz.eckardt@brz.eu>
2023-06-15 10:14:31 +0200

    Upload convo

--- convo.py
+++ convo.py
@@ -4,8 +4,8 @@ import numpy as np
 
 # Create gaussian kernel according to the formula in the exercise sheet
 def make_kernel(ksize, sigma):
-    # Initialize the kernel
-    kernel = np.zeros((ksize, ksize))
+    kernel = np.zeros((ksize, ksize), np.float32)
+    center = ksize // 2
 
     # Apply the formula for each element in the kernel
     for x in range(ksize):
@@ -28,45 +28,51 @@ def slow_convolve(arr, k):
     # Get kernel dimensions
     kernel_height, kernel_width = k.shape
 
-    # Calculate the padding
+    # Calculate padding
     padding_height = kernel_height // 2
     padding_width = kernel_width // 2
 
-    # Apply padding to the image without using numpy
-    padded_image = np.zeros((image_height + 2 * padding_height, image_width + 2 * padding_width))
-
-    # Create result image
-    result_image = np.zeros((image_height, image_width))
-
-    # Flip
+    # Flip the kernel (Asked for allowance)
     flipped_kernel = np.flipud(np.fliplr(k))
 
+    # Apply padding to the image without using numpy
+    padded_image = np.zeros((image_height + 2 * padding_height, image_width + 2 * padding_width))
+    padded_image[padding_height:padding_height + image_height, padding_width:padding_width + image_width] = arr
+
+    # Create convolved image
+    convolved_image = np.zeros((image_height, image_width))
+
+    # Apply convolution
     for i in range(image_height):
         for j in range(image_height):
             for u in range(kernel_height):
                 for v in range(kernel_width):
-                    result_image[i, j] += flipped_kernel[u, v] * padded_image[i + u, j + v]
+                    convolved_image[i, j] += flipped_kernel[u, v] * padded_image[i + u, j + v]
 
-    # Return result image
-    return result_image
+    # Return the convolved image
+    return convolved_image
 
 
 if __name__ == '__main__':
     # Find best parameters for the kernel (ksize and sigma)
-    k = make_kernel(3, 2)
-    print(k)
+    k = make_kernel(9, 2)
 
-    # Test the convolution
+    # Test convolution
     a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    b = np.array([[1, 0, 0], [0, 0, 0], [0, 0, 0]])
-    slow_convolve(a, b)
+    b = np.array([[2]])
+    test_image = slow_convolve(a, b)
 
     # Load image
     im = np.array(Image.open('input1.jpg'))
-    # im = np.array(Image.open('input2.jpg'))
-    # im = np.array(Image.open('input3.jpg'))
+    # im = np.array(Image.open('input2.jpg').convert('L'))
+    # im = np.array(Image.open('input3.jpg').convert('L'))
 
-    # TODO: blur the image, subtract the result to the input,
-    #       add the result to the input, clip the values to the
-    #       range [0,255] (remember warme-up exercise?), convert
-    #       the array to np.unit8, and save the result
+    # Convolve image
+    conv_img = slow_convolve(im, k)
+
+    # Apply sharpening and show image
+    result = im + (im - conv_img)  # Blur the image, subtract the result to the input, add the result to the input
+    result = np.clip(result, 0, 255)  # Range [0,255] (remember warme-up exercise?)
+    image = Image.fromarray(result.astype(np.uint8))  # Convert the array to np.unit8, and save the result
+    image.show()
+
